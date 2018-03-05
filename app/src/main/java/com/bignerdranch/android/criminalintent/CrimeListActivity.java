cfdf85f9bbc0512fc49708cc9301848330b8c4a9
Param <param_jain@icloud.com>
2018-03-05 16:00:27 +0530

    First commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,44 @@
+package com.bignerdranch.android.criminalintent;
+
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by param_jain on 24/11/17.
+ **/
+
+public class CrimeListActivity extends SingleFragmentActivity implements CrimeListFragment.Callbacks {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+
+    @Override
+    protected int getLayoutResId() { return R.layout.activity_masterdetail; }
+
+    @Override
+    public void onCrimeSelected(Crime crime) {
+        if(findViewById(R.id.detail_fragment_container) == null) {
+            Intent intent = CrimePagerActivity.newIntent(this, crime.getId());
+            startActivity(intent);
+        } else {
+            Fragment newDetail = CrimeFragment.newInstance(crime.getId());
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.detail_fragment_container, newDetail)
+                    .commit();
+
+        }
+    }
+
+    public void onCrimeUpdated (Crime crime) {
+        CrimeListFragment listFragment = (CrimeListFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_container);
+        listFragment.updateUI();
+    }
+}
+
